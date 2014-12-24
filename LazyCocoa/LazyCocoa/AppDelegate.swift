7afc6912a07976fcdce91b28b1bf5c95338a6ddd
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-12-25 04:47:24 +1100

    Removed _* and ignored DS_Store files

---
+++ LazyCocoa/LazyCocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LazyCocoa
+//
+//  Created by Yichi on 25/12/2014.
+//  Copyright (c) 2014 Yichi Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ LazyCocoa/LazyCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LazyCocoa
+//
+//  Created by Yichi on 25/12/2014.
+//  Copyright (c) 2014 Yichi Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
