e5974b342de3fbec2a5ab14617a3a8126c59855a
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-12-27 08:03:03 +1100

    Tracking StatementModel

---
+++ LazyCocoa-MacApp/LazyCocoa-MacApp/Core/StatementModel.swift
@@ -0,0 +1,60 @@
+/*
+
+Copyright (c) 2014 Yichi Zhang
+https://github.com/yichizhang
+zhang-yi-chi@hotmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+import Cocoa
+
+enum GenerationMode : String {
+	case ObjC = "objc"
+	case Swift = "swift"
+}
+
+protocol CanBeConvertedToObjC {
+	func objcHeaderStringWithoutSemicolon() ->String;
+	func objcHeaderString() ->String;
+	func objcImplementationString() ->String;
+}
+
+protocol CanBeConvertedToSwift {
+	func swiftString() ->String;
+}
+
+class StatementModel : NSObject {
+	
+	var statementString:String!
+	
+	private(set) var identifier:String?
+	var color:ColorModel?
+	var font:FontModel?
+	
+	convenience init(string:String){
+		
+		self.init()
+		
+		self.statementString = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+		
+		let componentsSeparatedBySpace = self.statementString.componentsSeparatedByString(" ")
+		
+		self.identifier = componentsSeparatedBySpace[0]
+		
+		let string = componentsSeparatedBySpace[1]
+		
+		if string.hasPrefix("#") {
+			self.color = ColorModel(colorHexString: string)
+		}else {
+			self.color = ColorModel()
+		}
+		
+	}
+}
+
