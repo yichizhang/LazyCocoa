e918e43e15e09c0e4442389c6e21606d69e50c54
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-22 23:19:57 +1100

    Finished building the header comment changer user interface.

---
+++ LazyCocoa/Source/LazyCocoa-MacApp/ChangeHeaderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChangeHeaderViewController.swift
+//  LazyCocoa-MacApp
+//
+//  Created by Yichi on 22/03/2015.
+//  Copyright (c) 2015 Yichi Zhang. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ChangeHeaderViewController : NSViewController, NSSplitViewDelegate {
+	
+	
+}
