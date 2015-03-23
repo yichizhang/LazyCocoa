5902d8634c4051e1b60b2df25b13660f2019f7ce
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-23 19:06:33 +1100

    Testing with table view

--- LazyCocoa/Source/LazyCocoa-MacApp/ChangeHeaderViewController.swift
+++ LazyCocoa/Source/LazyCocoa-MacApp/ChangeHeaderViewController.swift
@@ -9,7 +9,33 @@
 import Foundation
 import AppKit
 
-class ChangeHeaderViewController : NSViewController, NSSplitViewDelegate {
+class ChangeHeaderViewController : NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 	
+	@IBOutlet weak var fileTableView: NSTableView!
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		fileTableView.setDataSource(self)
+		fileTableView.setDelegate(self)
+		
+		fileTableView.reloadData()
+	}
+	
+	func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+		return 10
+	}
+	
+	func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+		
+		if let tableColumn = tableColumn {
+			if let cellView = tableView.makeViewWithIdentifier("Cell", owner: self) as? NSTableCellView {
+				cellView.textField?.stringValue = "\(row)\(tableColumn.title)"
+				
+				return cellView
+			}
+		}
+		return nil
+	}
 }
+
