e7920b6819878e0b4887bffbe72bc6ce9b4a8b15
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-04-11 16:15:40 +1000

    Added comments to ColorScanner

--- LazyCocoa/Source/LazyCocoa-MacApp/Core/ColorScanner.swift
+++ LazyCocoa/Source/LazyCocoa-MacApp/Core/ColorScanner.swift
@@ -24,35 +24,54 @@ class ColorScanner: NSObject {
 		
 		var returnString:String = ""
 		
+		// Scan the input string
 		while ( scanner.scanLocation < count(scanner.string) ) {
 			
+			// Get the character at the current scan location as a String.
 			let charString = (scanner.string as NSString).substringWithRange(
 				NSMakeRange(scanner.scanLocation, 1)
 			)
 			
 			if charString.containsCharactersInSet(decDigitSet) {
+				// The character is a digit.
+				// It means that there is a color component.
+				// Get its value by using `scanFloat`
 				scanner.scanFloat(&resultFloat)
 				
+				// Put the color component in the array
 				currentColorComponents.append( CGFloat(resultFloat) )
 				
 			} else if charString.containsCharactersInSet(NSCharacterSet.newlineCharacterSet()) {
-				// New line.
+				// The character is a new line character.
 				
 				if currentColorComponents.count >= 3 {
+					// We get a new line and there are already 3 color components in the array;
+					// it looks like we got all the color components for the current color already.
+					// So we out put the current color to `returnString` and delete it from memory
+					// to make room for the next color.
 					let hexString = ColorFormatter.hexStringFrom(componentArray: currentColorComponents)
 					if let colorName = currentColorName {
+						// Out put the color
 						returnString = returnString + "\(colorName) \(hexString)\n"
 					}
 					
+					// Clear current color components array and current color name.
 					currentColorComponents.removeAll()
 					currentColorName = nil
 				}
 				
+				// Increment scan location, because we haven't scanned anything.
 				scanner.scanLocation++
 				
 			} else if scanner.scanUpToCharactersFromSet(set1, intoString: &resultString) {
+				// The character is neither a digit or a new line character.
+				// Scan up to an Objective-C special character (at the moment, `set1`)
 				
 				if let resultString = resultString {
+					
+					// Check the string scanned and see if it is a color name.
+					// At the moment, the criteria is that it ends with `Color` 
+					// but not `UIColor` or `NSColor`
 					if resultString.hasSuffix("Color") &&
 					resultString != "UIColor" &&
 					resultString != "NSColor" {
@@ -62,6 +81,7 @@ class ColorScanner: NSObject {
 				}
 				
 			} else {
+				// Keep scanning
 				scanner.scanLocation++
 			}
 			
