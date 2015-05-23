c5c7f132528f7290b5e47a5049120335658cb1a9
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-05-23 12:44:10 +1000

    Added target for the command line tool

---
+++ LazyCocoa/Source/lzyc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lzyc
+//
+//  Created by Yichi on 23/05/2015.
+//  Copyright (c) 2015 Yichi Zhang. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
